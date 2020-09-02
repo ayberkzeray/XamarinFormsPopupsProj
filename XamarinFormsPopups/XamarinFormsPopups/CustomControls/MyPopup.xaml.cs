91a3bdb9c01bda24b3e1c023fc0c0d7054097c08
Ayberk ZERAY <ayberkzeray@gmail.com>
2020-09-02 17:51:10 +0300

    Add project files.

---
+++ XamarinFormsPopups/XamarinFormsPopups/CustomControls/MyPopup.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsPopups.CustomControls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyPopup : PopupPage
+    {
+        public MyPopup(string title, string message, string okText="OKEY")
+        {
+            InitializeComponent();
+
+            boldLabel.Text = title;
+            boldLabel.IsVisible = !string.IsNullOrEmpty(title);
+            descriptionLabel.Text = message;
+            okButton.Text = okText;
+        }
+
+        public void Ok_Clicked(object sender, System.EventArgs e)
+        {
+            if (PopupNavigation.Instance.PopupStack.Count > 0)
+            {
+                PopupNavigation.Instance.PopAsync();
+            }
+        }
+
+    }
+}
---
+++ XamarinFormsPopups/XamarinFormsPopups/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinFormsPopups.CustomControls;
+
+namespace XamarinFormsPopups
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            MyPopup myPopup = new MyPopup(title:"This is title","This is my message to the world: Lorem impsum","OKEY");
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await PopupNavigation.Instance.PushAsync(myPopup, true);
+            });
+        }
+    }
+}
